5e176b0f5decf01ed0fd785e81362981962e59fa
Cemal Demir <CMLDMR@users.noreply.github.com>
2023-05-03 16:15:26 +0300

    DB Conntection

--- main.cpp
+++ main.cpp
@@ -1,19 +1,11 @@
 
 #include <Wt/WApplication.h>
-
 #include <mongocxx/instance.hpp>
-
 #include "mainapplication.h"
-
-
-using namespace std;
 
 int main(int argc, char *argv[])
 {
-
     mongocxx::instance instance{};
-
-
     return Wt::WRun(argc,argv,[](const Wt::WEnvironment &env){
         return Wt::cpp14::make_unique<MainApplication>(env);
     });
--- mainapplication.h
+++ mainapplication.h
@@ -6,6 +6,9 @@
 #include <Wt/WEnvironment.h>
 #include <Wt/WTheme.h>
 #include <Wt/WBootstrap3Theme.h>
+#include <mongocxx/database.hpp>
+#include <mongocxx/client.hpp>
+
 
 
 class MainApplication : public Wt::WApplication
@@ -13,7 +16,13 @@ class MainApplication : public Wt::WApplication
 public:
     MainApplication(const Wt::WEnvironment &env);
 
+
+
+private:
     std::shared_ptr<Wt::WBootstrap3Theme> mTheme;
+
+    mongocxx::client* mClient;
+    mongocxx::database mDB;
 };
 
 #endif // MAINAPPLICATION_H
